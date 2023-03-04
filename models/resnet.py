f6112f3fccd499cdc82076558ccbe6ba8bcc3bc4
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-04 21:49:25 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -84,10 +84,10 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-        print('\n\nWeight and Bias parameters:')
+        '''print('\n\nWeight and Bias parameters:')
         for param in self.linear.parameters():
             print(param)
-            print("The shape:",param.shape)
+            print("The shape:",param.shape)'''
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
