bf6a0d4194ae7fd12e6e3c59ab110415051336a3
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-04 18:43:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ trainloader = torch.utils.data.DataLoader(
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=1, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
