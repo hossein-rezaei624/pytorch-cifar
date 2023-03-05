8b1a32e732ad9bf9b4e9cfec000da5e1dfbb16cc
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-05 19:18:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,9 @@ def test(epoch):
     count = 0
     with torch.no_grad():
         #img, label = testset[0]
-        xx, yy = next(iter(testloader))
+        
+        for i in range(100):
+          xx, yy = next(iter(testloader))
         img, label = next(iter(testloader))
         print("img shape:",img.shape,"label",label)
         img, label = img.to(device), label.to(device)
