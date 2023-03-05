bd05352462585492b2fc1a8a5f39c170fa5c4f61
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-05 21:31:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -184,8 +184,8 @@ def test(epoch):
           print("The angle with the weights of the class",i," is:",angle*57.2958)
           
           
-          print(img[0])
-          plt.imshow(img[0].permute(1, 2, 0).cpu(), vmin=0, vmax=255)
+        print(img[0])
+        plt.imshow(img[0].permute(1, 2, 0).cpu(), vmin=0, vmax=255)
         #//////////////////////////////////////////////////
         
         
