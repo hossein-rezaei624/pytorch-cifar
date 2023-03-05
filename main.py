105509d7eb6f0bbb09000d8deb89e87d791980ab
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-05 22:15:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -185,9 +185,9 @@ def test(epoch):
           
           
         #print(img[0][0])
-        plt.imshow((np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1)*255.0).astype(np.uint8))
+        plt.imsave("./image.png",(np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1)*255.0).astype(np.uint8))
         #print(np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1).max(),np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1).min())
-        plt.show()
+        #plt.show()
         #//////////////////////////////////////////////////
         
         
