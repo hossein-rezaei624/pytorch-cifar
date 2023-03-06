7261c219b0c78818c01fab8ad857f8aff2c468ed
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-06 16:26:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -43,12 +43,12 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.CIFAR100(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.CIFAR100(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
@@ -153,7 +153,7 @@ def test(epoch):
         print("img shapeeeeeee:",img.shape,"label",label)
         img, label = img.to(device), label.to(device)
         #img = img.rotate(90)
-        img = torchvision.transforms.functional.rotate(img, 90)
+        ###img = torchvision.transforms.functional.rotate(img, 90)
         outputs, rep = net(img)
         loss = criterion(outputs, label)
         test_loss += loss.item()
