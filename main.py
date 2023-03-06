0098eb978242671862383d8c8b7f9c7672ae9ec2
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-06 18:15:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -43,12 +43,12 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR100(
+trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR100(
+testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
@@ -147,9 +147,9 @@ def test(epoch):
         
         '''for i in range(324):
           img, label = next(iter(testloader))'''
-        img, label = next(iter(testloader))
+        img, label = next(iter(trainloader))
         print("img shape:",img.shape,img[0].shape,"label",label)
-        img, label = img[83].view((1,3,32,32)), label[83].view((1))
+        img, label = img[0].view((1,3,32,32)), label[0].view((1))
         print("img shapeeeeeee:",img.shape,"label",label)
         img, label = img.to(device), label.to(device)
         #img = img.rotate(90)
