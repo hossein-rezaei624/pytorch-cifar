380ef4c61062704689658325c7c25b3870d89843
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-06 12:36:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def test(epoch):
         print("ggggggggggggggggg",(img[0].permute(1,2,0).cpu().numpy()).max(),(img[0].permute(1,2,0).cpu().numpy()).min())
         aa_ = ((((((img[0].permute(1,2,0).cpu().numpy())-(img[0].permute(1,2,0).cpu().numpy()).mean())/((img[0].permute(1,2,0).cpu().numpy()).std()))*255.0).astype(np.uint8)))
         #plt.imsave("./image.png",(np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1)*255.0).astype(np.uint8))
-        plt.imsave("./image.png",img[0].permute(1,2,0).cpu().numpy())
+        plt.imsave("./image.png", aa_, vmin = 0, vmax = 255)
         #print(np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1).max(),np.clip((img[0].permute(1,2,0).cpu().numpy()), 0, 1).min())
         #plt.show()
         #//////////////////////////////////////////////////
