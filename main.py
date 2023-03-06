adaaa380cbf25da713083fa7f152216f50c326ca
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-06 10:45:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def test(epoch):
           img, label = next(iter(testloader))'''
         img, label = next(iter(testloader))
         print("img shape:",img.shape,img[0].shape,"label",label)
-        img, label = img[8].view((1,3,32,32)), label[8].view((1))
+        img, label = img[91].view((1,3,32,32)), label[91].view((1))
         print("img shapeeeeeee:",img.shape,"label",label)
         img, label = img.to(device), label.to(device)
         outputs, rep = net(img)
