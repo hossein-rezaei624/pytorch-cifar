f25440a4a70f48e7d3bb0517f69e0b684258d049
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-08 16:00:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def train(epoch):
         temp_2 = sum(temp_1)
         sum_1 = sum(sum_)
         print("jjjjjjjjjjjj",0.0001*temp_2,"hhhhhhhhh",100000/sum_1)
-        loss = criterion(outputs, targets) + (100000/sum_1)
+        loss = criterion(outputs, targets) + (100000/sum_1) + (0.0001*temp_2)
         loss.backward()
         optimizer.step()
 
