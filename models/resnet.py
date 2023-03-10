622904c26978a795489e1a5818fac95527983bad
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-10 13:44:36 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -99,8 +99,19 @@ class ResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
+        out1 = out.view(out.size(0), -1)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        out = self.linear(out1)
         return out
 
 
