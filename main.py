20eaf2aa07e596a41f701f2c817931b29bdc8f24
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-14 03:21:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -108,10 +108,10 @@ def train(epoch):
         print("jjjj",temp.shape,(torch.matmul(A.transpose(1,2),rep)).shape)
         temp = temp.view(128,32)
         print("kkkkk",(((temp.pow(2)).sum(1))).shape)
-        temp = ((temp.pow(2)).sum(1))**0.5
-        yy = torch.ones(128, dtype=float).to("cuda")
+        temp = (((((temp.pow(2)).sum(1))**0.5).pow(2)).sum(0))**0.5
+        #yy = torch.ones(128, dtype=float).to("cuda")
         loss = criterion(outputs, targets) + temp
-        loss.backward(yy)
+        loss.backward()
         optimizer.step()
 
         train_loss += loss.item()
