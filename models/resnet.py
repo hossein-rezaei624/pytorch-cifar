0913ff5bfc9ad67c16cad5d352d1989100f21d3f
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-13 22:10:28 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -103,6 +103,8 @@ class ResNet(nn.Module):
         print(out.shape)
         out = out.view(out.size(0), -1)
         print(out.shape)
+        out = F.avg_pool2d(out, 64)
+        print(out.shape)
         
         oo = nn.Linear(out.shape[1],(512,512))
         out1 = oo(out)
