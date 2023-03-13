e00f924a44a33c2718696918e2fa0a1af406fb4d
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-14 03:07:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -103,6 +103,7 @@ def train(epoch):
         print("wwwww",outputs.shape,rep.shape,A.shape)
         A = A.view(128,32,32)
         print(A.transpose(1,2).shape)
+        rep = rep.view(128,32,1)
         temp = rep - torch.matmul(A.transpose(1,2),rep)
         temp = ((temp.pow(2)).sum(0))**0.5
         loss = criterion(outputs, targets) + temp
