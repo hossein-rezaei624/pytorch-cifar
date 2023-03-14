d1bfa72060518ae7fe76f6c2ac13a24d595b7531
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-03-14 04:05:31 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -82,7 +82,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(1024*block.expansion, num_classes)
+        self.linear = nn.Linear(32*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -121,11 +121,16 @@ class ResNet(nn.Module):
         print(out2.shape,"oooo")
         
         
+        A = out2.view(out2.shape[0],32,32)
+        #print(A.transpose(1,2).shape)
+        rep = out.view(out2.shape[0],32,1)
+        temp = torch.matmul(A.transpose(1,2),rep)
+        #print("jjjj",temp.shape,(torch.matmul(A.transpose(1,2),rep)).shape)
+        temp = temp.view(out2.shape[0],32)
         
         
         
-        
-        out3 = self.linear(out2)
+        out3 = self.linear(temp)
         return out3, out, out2
 
 
