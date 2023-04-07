9f386ca6eb117ea5fae3a4c1ba562a0055bfb8bc
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-07 21:42:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, 768, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        progress_bar(batch_idx, 6, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
         
         #print(batch_idx)
@@ -134,7 +134,7 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, 6, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
