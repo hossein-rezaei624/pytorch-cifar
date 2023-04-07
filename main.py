bf79dc4211c586a13f838eded9e7526d95addad1
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-07 21:37:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            progress_bar(batch_idx, 6, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
