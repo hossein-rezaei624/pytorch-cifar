8c0e70b031f903a508af5451c3f21df2d874e9ed
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-07 21:24:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
-print(trainloader,"\n",len(trainloader),len(trainloader[:100]))
+print(trainloader,"\n",len(trainloader))
 # Training
 def train(epoch):
     print('\nEpoch: %d' % epoch)
