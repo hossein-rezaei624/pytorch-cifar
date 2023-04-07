299f03c98bd99b351959e826d4980ac2c927039c
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-07 21:16:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=False, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
@@ -111,6 +111,9 @@ def train(epoch):
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        
+        if batch_idx == 6:
+          break
 
 
 def test(epoch):
