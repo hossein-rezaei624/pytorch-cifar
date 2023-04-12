5414ffa597dc76ab745ae77e748a18409d94b79e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-13 01:32:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ def test(epoch):
             sum_2_4 = sum(sum(temp44))
           ###print("the sum of the false angle is:", sum_2)
           ####print((sum_1_3 + sum_2_4))
-          final_ = ((1.5*sum_1 + sum_2)/correct) + ((0.5*sum_1_3 + sum_2_4)/((100 - correct)+0.0000001))
+          final_ = ((0.5*sum_1 + sum_2)/correct) + ((1.5*sum_1_3 + sum_2_4)/((100 - correct)+0.0000001))
           ####print("Final:",final_)
           
           some_new.append(final_)
