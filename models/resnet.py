bb4213fb2b66f174b048f3cbbda88aa6c6b03760
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-12 16:57:31 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -105,11 +105,11 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out1 = out.view(out.size(0), -1)
-        print("The representation:",out1)
-        print("The shape of the representation:",out1.shape)
+        ###print("The representation:",out1)
+        ###print("The shape of the representation:",out1.shape)
         out = self.linear(out1)
         #print("last layer output:",out)
-        print("The shape of the last layer output:",out.shape)
+        ###print("The shape of the last layer output:",out.shape)
         return out, out1
 
 
