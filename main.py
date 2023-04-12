a35931b0e48a997842e7ad40f869498908c4911e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-12 18:19:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ transform_train = transforms.Compose([
 ])
 
 transform_test = transforms.Compose([
-    transforms.RandomRotation(90),
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
