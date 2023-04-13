6a64760435f127de70ef78141635f9c3734bafa4
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-13 21:04:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def train(epoch):
         #print("targets.shape[0]",targets.shape[0])
         for i in range(targets.shape[0]):
           if targets[i] == 0:
-            inputs[i] = torch.tensor(random_noise(inputs[i], mode='salt', amount=0.005))
+            inputs[i] = torch.tensor(random_noise(inputs[i], mode='salt', amount=0.001))
         inputs, targets = inputs.to(device), targets.to(device)
         
         optimizer.zero_grad()
