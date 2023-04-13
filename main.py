9d5c4d82cbee66e63aeb6471d503a28c27e02d70
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-13 14:43:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
+    transforms.ElasticTransform()
+    transforms.RandomEqualize(p=0.5)
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
+    transforms.RandomRotation(90)
+    transforms.RandomAffine(90)
+    transforms.ColorJitter(brightness=(0,1), contrast=(0,1), saturation=(0,1), hue=(-0.5,0.5))
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
