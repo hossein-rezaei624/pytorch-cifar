6ce742adde5d0a64ef86570a19922fa826ba41d3
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-13 19:28:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
+    transforms.RandomRotation(90),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -96,7 +99,7 @@ def train(epoch):
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         
-        inputs = torch.tensor(random_noise(inputs, mode='salt', amount=0.02))
+        #inputs = torch.tensor(random_noise(inputs, mode='salt', amount=0.02))
         inputs, targets = inputs.to(device), targets.to(device)
         
         optimizer.zero_grad()
