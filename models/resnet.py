5601114be6b750302b82857cc3589b2e53e02d3b
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-15 16:52:09 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -95,8 +95,8 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
-        out = self.layer2(out)
-        out = F.dropout(out)
+        #out = self.layer2(out)
+        
         #print("shape of layer 2:", out.shape)
         #out = self.layer3(out)
         #print("shape of layer 3:", out.shape)
@@ -104,6 +104,7 @@ class ResNet(nn.Module):
         #print("shape of layer 4:", out.shape)
         out = F.avg_pool2d(out, 4)
         out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 2)
         #print("shape after doing avgpool:", out.shape)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
