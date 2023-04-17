c9023f1c4f32ddb22ed747a35e97471cb061b732
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-17 12:13:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomEqualize(p=0.3),
-    transforms.ElasticTransform(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -55,7 +53,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = ResNet34()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
