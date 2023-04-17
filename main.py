a7644aa70f8a27fb8b1e7258221b3a017c172841
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-17 12:11:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,6 @@ print('==> Preparing data..')
 transform_train = transforms.Compose([
     transforms.RandomEqualize(p=0.3),
     transforms.ElasticTransform(),
-    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-    transforms.RandomAffine(30),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
