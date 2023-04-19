3eb9c8f7e104c31010dad43d10dc79dfa7e9ee07
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-20 00:01:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(90),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -55,7 +54,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = ResNet18()
+net = DLA()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -142,7 +141,7 @@ def test(epoch):
         'acc': acc,
         'epoch': epoch,
     }
-    torch.save(state, '/content/drive/MyDrive/angle_models/others/10/ckpt.pth')
+    torch.save(state, '/content/drive/MyDrive/angle_models/others/11/ckpt.pth')
     
     if acc > best_acc:
         print('Saving..')
