88c1c6f60054b729c38fc57eeb1a9ccde1c8de85
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-19 23:54:33 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -96,17 +96,9 @@ class ResNet(nn.Module):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
-        
-        #print("shape of layer 2:", out.shape)
         out = self.layer3(out)
-        #print("shape of layer 3:", out.shape)
         out = self.layer4(out)
-        #print("shape of layer 4:", out.shape)
         out = F.avg_pool2d(out, 4)
-        #out = F.avg_pool2d(out, 4)
-        #out = F.avg_pool2d(out, 2)
-        #out = F.avg_pool2d(out, 32)
-        #print("shape after doing avgpool:", out.shape)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
