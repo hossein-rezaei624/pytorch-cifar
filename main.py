b2d2588b4c0d570ea58bc020cbb1624e8cc9c8ab
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-20 02:13:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,8 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomEqualize(p=0.25),
-    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-    transforms.RandomAffine(45),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -56,10 +55,10 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = DLA()
+#net = DLA()
 # net = PreActResNet18()
 # net = GoogLeNet()
-# net = DenseNet121()
+net = DenseNet121()
 # net = ResNeXt29_2x64d()
 # net = MobileNet()
 # net = MobileNetV2()
@@ -143,7 +142,7 @@ def test(epoch):
         'acc': acc,
         'epoch': epoch,
     }
-    torch.save(state, '/content/drive/MyDrive/angle_models/others/13/ckpt.pth')
+    torch.save(state, '/content/drive/MyDrive/angle_models/others/14/ckpt.pth')
     
     if acc > best_acc:
         print('Saving..')
