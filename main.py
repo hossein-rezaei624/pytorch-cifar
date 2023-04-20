e3f8dbfd0bc5ba3ec85cd3c7c32bf0a997d9d5e1
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-21 00:47:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomRotation(90),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
