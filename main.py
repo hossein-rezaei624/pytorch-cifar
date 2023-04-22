8f5cf8ab41f3f44cef64b40cb46e9fc071606776
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-22 19:23:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(90),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -58,7 +57,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = ResNet18()
+net = DLA()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -125,11 +124,11 @@ tempp = 0
 #bias_ = torch.zeros((10))
 for param in net.parameters():
     tempp +=1
-    if (tempp==61):
+    if (tempp==131):
       ###print(param)
       ###print("the shapeeeeeee",param.shape)
       weights_ = param
-    if (tempp==62):
+    if (tempp==132):
       ###print(param)
       ###print("the shapeeeeeee",param.shape)
       bias_ = param
