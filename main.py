e4326d2ef90036d97ef0a97e3ce59954cbe89ddc
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-27 15:40:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def test(epoch):
           SoftMax_ = nn.functional.softmax(outputs, dim = 1)
           #print('SoftMax_.shape',SoftMax_.shape)
           #print('SoftMax_',SoftMax_)
-          SoftMax_predicted = SoftMax_.max(1)
+          SoftMax_predicted, indices = SoftMax_.max(1)
           print("SoftMax_predicted",SoftMax_predicted)
           print("SoftMax_predicted.shape",SoftMax_predicted.shape)
           
