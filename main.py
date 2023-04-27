d001c047bb54f0908a853fa243f553c20ecf6be2
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-27 16:02:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -248,6 +248,7 @@ def test(epoch):
         
         #print("SoftMax of truly classified images:", soft11)
         #print("SoftMax of misclassified images:", soft33)
+        print("lennnn",len(soft22), len(soft44))
         print("true class SoftMax:",sum(soft22)/(batch_idx+1))
         print("fals class SoftMax:",sum(soft44)/(batch_idx+1))
 
