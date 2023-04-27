277711f45df14680358e60e52af39054a20088ba
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-27 18:26:12 +0330

    Update mobilenetv2.py

--- models/mobilenetv2.py
+++ models/mobilenetv2.py
@@ -72,9 +72,9 @@ class MobileNetV2(nn.Module):
         out = F.relu(self.bn2(self.conv2(out)))
         # NOTE: change pooling kernel_size 7 -> 4 for CIFAR10
         out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
+        out1 = out.view(out.size(0), -1)
+        out = self.linear(out1)
+        return out, out1
 
 
 def test():
