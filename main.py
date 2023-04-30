18b3a36471b040e793fb23238a7144850afbe8b1
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-04-30 21:46:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def test(epoch):
         'acc': acc,
         'epoch': epoch,
     }
-    torch.save(state, f'/content/drive/MyDrive/final/13/ckpt{epoch}.pth')    
+    torch.save(state, f'/content/drive/MyDrive/final/14/ckpt{epoch}.pth')    
     
     if acc > best_acc:
         print('Saving..')
