23b830d14ca1483d4925de8ee3c83b11fc458e30
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-06 21:16:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -53,8 +54,8 @@ testloader = torch.utils.data.DataLoader(
 
 # Model
 print('==> Building model..')
-net = VGG('VGG16')
-#net = ResNet18()
+#net = VGG('VGG16')
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
