581ebb7c291c9704bb37e80565eabffbe5f64fe7
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-06 10:11:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,6 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -57,13 +54,13 @@ testloader = torch.utils.data.DataLoader(
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-#net = DLA()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
 # net = ResNeXt29_2x64d()
 # net = MobileNet()
-net = MobileNetV2()
+#net = MobileNetV2()
 # net = DPN92()
 # net = ShuffleNetG2()
 # net = SENet18()
@@ -145,7 +142,7 @@ def test(epoch):
         'acc': acc,
         'epoch': epoch,
     }
-    torch.save(state, f'/content/drive/MyDrive/final/SVHN/4/ckpt{epoch}.pth')    
+    torch.save(state, f'/content/drive/MyDrive/final/SVHN/5/ckpt{epoch}.pth')    
     
     if acc > best_acc:
         print('Saving..')
