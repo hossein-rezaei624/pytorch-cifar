56556b53a25bfd6a83d2145626fc06164e779c8e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-06 17:47:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ trainset = torchvision.datasets.SVHN(
     root='./data', split = 'train', download=True, transform=transform_train)
 
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=False, num_workers=2)
 
 testset = torchvision.datasets.SVHN(
     root='./data', split = 'test', download=True, transform=transform_test)
