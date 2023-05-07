4af86c91398c8d4c59b053c2d969c8786910915e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-08 02:58:17 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,5 +1,7 @@
 '''ResNet in PyTorch.
+
 For Pre-activation ResNet, see 'preact_resnet.py'.
+
 Reference:
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
     Deep Residual Learning for Image Recognition. arXiv:1512.03385
@@ -22,7 +24,6 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
-        self.drop = nn.Dropout()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
@@ -35,8 +36,8 @@ class BasicBlock(nn.Module):
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
-        out = self.drop(out)
         return out
+
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -81,7 +82,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(256*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -96,8 +97,9 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
+        #out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 2)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
