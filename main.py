4e533c28590c84e73546b1a71b7cfd169494cc70
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-08 03:04:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ testloader = torch.utils.data.DataLoader(
 # Model
 print('==> Building model..')
 #net = VGG('VGG19')
-net = ResNet34()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 #net = DenseNet121()
@@ -141,7 +141,7 @@ def test(epoch):
         'acc': acc,
         'epoch': epoch,
     }
-    torch.save(state, f'/content/drive/MyDrive/final/CIFAR100_/10/ckpt{epoch}.pth')    
+    torch.save(state, f'/content/drive/MyDrive/final/CIFAR100_/11/ckpt{epoch}.pth')    
     
     if acc > best_acc:
         print('Saving..')
