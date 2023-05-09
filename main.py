9e3dac33a113e6b3f41d95f22e4c99df01642b6a
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-09 14:13:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
