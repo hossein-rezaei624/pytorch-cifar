ef5b542d614571ffe0b2a6d041519085c06e7260
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-09 22:53:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(90),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -41,14 +43,13 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.SVHN(
-    root='./data', split = 'train', download=True, transform=transform_train)
-
+trainset = torchvision.datasets.CIFAR100(
+    root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=False, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.SVHN(
-    root='./data', split = 'test', download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR100(
+    root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
@@ -59,7 +60,7 @@ testloader = torch.utils.data.DataLoader(
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = ResNet18()
+net = ResNet50()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
