8ea9d9ab4ae9cbe6769735ffed7dfb7caccb88ce
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-09 23:03:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -127,11 +127,11 @@ tempp = 0
 #bias_ = torch.zeros((10))
 for param in net.parameters():
     tempp +=1
-    if (tempp==61):
+    if (tempp==160):
       ###print(param)
       ###print("the shapeeeeeee",param.shape)
       weights_ = param
-    if (tempp==62):
+    if (tempp==161):
       ###print(param)
       ###print("the shapeeeeeee",param.shape)
       bias_ = param
