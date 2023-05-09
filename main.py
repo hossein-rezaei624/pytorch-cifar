c0956b77bfe87df6a1b8a033a02b3bfc7a93615f
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-09 16:31:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -45,7 +44,7 @@ trainset = torchvision.datasets.SVHN(
     root='./data', split = 'train', download=True, transform=transform_train)
 
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=False, num_workers=2)
 
 testset = torchvision.datasets.SVHN(
     root='./data', split = 'test', download=True, transform=transform_test)
