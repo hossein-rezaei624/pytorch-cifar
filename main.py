2d4457f44154dda473c25c6fac1be27651b25b2f
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-05-10 14:19:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(90),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
@@ -60,10 +57,10 @@ testloader = torch.utils.data.DataLoader(
 # Model
 print('==> Building model..')
 #net = VGG('VGG19')
-#net = ResNet18()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
-net = DenseNet121()
+#net = DenseNet121()
 # net = ResNeXt29_2x64d()
 # net = MobileNet()
 #net = MobileNetV2()
@@ -127,11 +124,11 @@ tempp = 0
 #bias_ = torch.zeros((10))
 for param in net.parameters():
     tempp +=1
-    if (tempp==361):
+    if (tempp==61):
       ###print(param)
       ###print("the shapeeeeeee",param.shape)
       weights_ = param
-    if (tempp==362):
+    if (tempp==62):
       ###print(param)
       ###print("the shapeeeeeee",param.shape)
       bias_ = param
