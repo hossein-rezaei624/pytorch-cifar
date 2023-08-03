c839c976d6ca9843dc0b2bf3d03f8e9bd4839cf6
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-08-04 00:41:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -149,8 +149,8 @@ def test(epoch):
             #print("......",cc.shape)
             #print("rep shape",rep.shape)
             #print("target_weighttttttttt",target_weight.view(512,1).shape)
-            print("rep.shapeeeee",rep.shape)
-            a = rep[0,:]
+            #print("rep.shapeeeee",rep.shape)
+            a = rep[0,:].view(1,512)
             #print(weights_.shape,"shapeeee")
             #b = cc
             b = target_weight.view(512,1)
