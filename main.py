11e70f028f7ecb65462a6aa5be4e718575fa51f7
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-08-05 20:18:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.CIFAR100(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.CIFAR100(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
@@ -60,7 +60,8 @@ testloader = torch.utils.data.DataLoader(
 
 # Model
 print('==> Building model..')
-net = ResNet18()
+#net = ResNet18()
+net = DenseNet121()
 #net = ResNet34()
 #net = ResNet50()
 #net = VGG('VGG19')
