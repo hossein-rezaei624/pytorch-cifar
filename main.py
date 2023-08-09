e22466932a3e1d5817675e39dd9fba3dcd8cfb87
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-08-09 18:55:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -39,13 +39,13 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.SVHN(
+    root='./data', split = 'train', download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.SVHN(
+    root='./data', split = 'test', download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
@@ -148,7 +148,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+50):
     train(epoch)
     test(epoch)
     scheduler.step()
