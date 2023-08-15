c25a4b41e5df0e2d068551c3b34ceef437eb76f4
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-08-15 10:14:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CelebA(
     root='./data', split = 'train', download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=8)
 
 testset = torchvision.datasets.CelebA(
     root='./data', split = 'test', download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=8)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
