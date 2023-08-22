3c2d6f2cbbb8ef4c7d380457f197cbcb0334be13
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-08-23 00:09:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import argparse
 
 from models import *
 from utils import progress_bar
+from kornia.augmentation import RandomResizedCrop, RandomHorizontalFlip, ColorJitter, RandomGrayscale
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -28,37 +29,65 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 
-sets = [
-    [26, 86, 2, 55, 75, 93, 16, 73, 54, 95],
-    [53, 92, 78, 13, 7, 30, 22, 24, 33, 8],
-    [43, 62, 3, 71, 45, 48, 6, 99, 82, 76],
-    [60, 80, 90, 68, 51, 27, 18, 56, 63, 74],
-    [1, 61, 42, 41, 4, 15, 17, 40, 38, 5],
-    [91, 59, 0, 34, 28, 50, 11, 35, 23, 52],
-    [10, 31, 66, 57, 79, 85, 32, 84, 14, 89],
-    [19, 29, 49, 97, 98, 69, 20, 94, 72, 77],
-    [25, 37, 81, 46, 39, 65, 58, 12, 88, 70],
-    [87, 36, 21, 83, 9, 96, 67, 64, 47, 44]
-]
+transform11 = nn.Sequential(
+    RandomResizedCrop(size = (32,32), scale=(0.2, 1.)),
+    RandomHorizontalFlip(),
+    ColorJitter(0.4, 0.4, 0.4, 0.1, p=0.8),
+    RandomGrayscale(p=0.2)
+)
 
-
-
-transform_test = transforms.Compose([
+transform_train = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+transform_test = transforms.Compose([
+    transforms.RandomRotation(90),
+    transforms.ToTensor(),
+])
+
+trainset = torchvision.datasets.CIFAR100(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR100(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+'''classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')'''
 
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
 net = Reduced_ResNet18(100)
-
+# net = PreActResNet18()
+# net = GoogLeNet()
+# net = DenseNet121()
+# net = ResNeXt29_2x64d()
+# net = MobileNet()
+# net = MobileNetV2()
+# net = DPN92()
+# net = ShuffleNetG2()
+# net = SENet18()
+# net = ShuffleNetV2(1)
+# net = EfficientNetB0()
+# net = RegNetX_200MF()
+# net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
-
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
@@ -68,32 +97,66 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr,
 checkpoint = torch.load('./checkpoint/ckpt.pth')
 net.load_state_dict(checkpoint['net'])
 
-
-
-
-dataset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
-net.eval()
-# Iterate over each set
-list11 = []
-for i, subset_classes in enumerate(sets):
-    # Get indices of samples belonging to the current subset
-    subset_indices = [idx for idx, (_, target) in enumerate(dataset) if target in subset_classes]
-
-    # Create a data loader for the current subset
-    subset_loader = torch.utils.data.DataLoader(torch.utils.data.Subset(dataset, subset_indices), batch_size=100, shuffle=False, num_workers=2)
-
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
     correct = 0
     total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        inputs = transform11(inputs)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
 
-    # Calculate accuracy for the current subset
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
     with torch.no_grad():
-        for data in subset_loader:
-            images, labels = data[0].to(device), data[1].to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
 
-    print(f'Set: {i}, Accuracy: {100 * correct / total}%')
-    list11.append(100 * correct / total)
-print("sum",sum(list11)/10)
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+
+test(epoch = 1)
+'''for epoch in range(start_epoch, start_epoch+75):
+    train(epoch)
+    test(epoch)
+    #scheduler.step()'''
