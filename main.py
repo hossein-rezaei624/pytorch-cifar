771627783d20f36412b08f21f91f8f623b84e71a
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-01 19:47:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,21 +28,19 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
+
+batch_size_ = 128
 
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=batch_size_, shuffle=False, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
@@ -96,10 +94,21 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
+    confidence_epoch = []
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
+        confidence_batch = []
+
+        for i in range(targets.shape[0]):
+          confidence_batch.append(outputs[i,targets[i]].item())
+        if (targets.shape[0] != batch_size_):
+          for j in range(batch_size_ - targets.shape[0]):
+            confidence_batch.append(0)
+        confidence_epoch.append(confidence_batch)
+        #print(len(confidence_epoch[0]))
+
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
@@ -111,7 +120,11 @@ def train(epoch):
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
+    
+    conf_tensor = torch.tensor(confidence_epoch)
+    conf_tensor = conf_tensor.reshape(conf_tensor.shape[0]*conf_tensor.shape[1])
+    conf_tensor = conf_tensor[:(total-1)]
+    print(conf_tensor.shape)
 
 def test(epoch):
     global best_acc
