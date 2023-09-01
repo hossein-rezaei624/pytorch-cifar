6dc6282c49d84ab815cb3f2b91f7936419995c39
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-09-01 19:47:41 +0330

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -83,6 +83,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.Soft = nn.Softmax(dim=1)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -101,6 +102,7 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
+        out = self.Soft(out)
         return out
 
 
