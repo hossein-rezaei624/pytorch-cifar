283e553657c8a70768f9dd42ed27f964cabd6ade
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-06 03:11:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR100(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=0)
+    trainset, batch_size=10, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR100(
     root='./data', train=False, download=True, transform=transform_test)
