97aa45a7467bf6f2c64f0e2c096b2eb01b97d38e
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-21 21:47:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 transform_train = transforms.Compose([transforms.ToTensor(),])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-filtered_indices = [i for i, label in enumerate(trainset.targets) if label in [2, 8]]
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+filtered_indices = [i for i, label in enumerate(trainset.targets) if label in [20, 90, 40]]
 filtered_data = torch.utils.data.Subset(trainset, filtered_indices)
 trainloader = torch.utils.data.DataLoader(filtered_data, batch_size=len(filtered_indices), shuffle=False)
 
 trainloader_ = torch.utils.data.DataLoader(filtered_data, batch_size=128, shuffle=True)
 
-mapping = {value: index for index, value in enumerate([2, 8])}
+mapping = {value: index for index, value in enumerate([20, 90, 40])}
 
 net = ResNet18()
 net = net.to(device)
