f1e4be8f87306b4f8176c56e019535ff1fec1bc6
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-23 02:45:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 
-transform_train = transforms.Compose([transforms.Resize((32, 32)), transforms.Lambda(lambda x: x.convert("RGB")), transforms.ToTensor(),])
+transform_train = transforms.Compose([transforms.ToTensor(),])
 
-trainset = torchvision.datasets.MNIST(root='./data', train = True, download=True, transform=transform_train)
+trainset = torchvision.datasets.SVHN(root='./data', split = 'train', download=True, transform=transform_train)
 filtered_indices = [idx for idx, (_, target, __) in enumerate(trainset) if target in [0, 1]]
 filtered_data = torch.utils.data.Subset(trainset, filtered_indices)
 trainloader = torch.utils.data.DataLoader(filtered_data, batch_size=len(filtered_indices), shuffle=False)
@@ -113,7 +113,7 @@ sorted_indices = np.argsort(Confidence_mean.numpy())
 
 top_indices = sorted_indices[-top_n:]
 
-#top_indices = top_indices[::-1]
+top_indices = top_indices[::-1]
 
 top_indices_sorted = top_indices[:100]
 
