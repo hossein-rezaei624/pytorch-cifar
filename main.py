fe0bf785af0a38fc90552a9a0ae3aa2029649808
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-23 02:41:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 transform_train = transforms.Compose([transforms.Resize((32, 32)), transforms.Lambda(lambda x: x.convert("RGB")), transforms.ToTensor(),])
 
 trainset = torchvision.datasets.MNIST(root='./data', train = True, download=True, transform=transform_train)
-filtered_indices = [idx for idx, (_, target) in enumerate(trainset) if target in [0, 1]]
+filtered_indices = [idx for idx, (_, target, __) in enumerate(trainset) if target in [0, 1]]
 filtered_data = torch.utils.data.Subset(trainset, filtered_indices)
 trainloader = torch.utils.data.DataLoader(filtered_data, batch_size=len(filtered_indices), shuffle=False)
 
