9218b97bf6ab6cbaae224c38ef8065352514b192
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-24 15:56:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ plt.savefig('scatter_plot.png')
 
 top_n = len(filtered_indices)
 
-sorted_indices = np.argsort(Variability.numpy())
+sorted_indices = np.argsort(Confidence_mean.numpy())
 
 top_indices = sorted_indices[-top_n:]
 
