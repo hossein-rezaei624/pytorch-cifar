c773c683b1fed73d3f0ed72eb2e1c3a4ed3ab917
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-10-24 15:38:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ from sklearn.manifold import TSNE
 import random
 
 
-np.random.seed(42)
-random.seed(42)
-torch.manual_seed(42)
+np.random.seed(1)
+random.seed(1)
+torch.manual_seed(1)
 if torch.cuda.is_available():
-  torch.cuda.manual_seed(42)
+  torch.cuda.manual_seed(1)
   torch.backends.cudnn.deterministic = True
   torch.backends.cudnn.benchmark = False
 
