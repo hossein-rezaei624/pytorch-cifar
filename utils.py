3a16187fd0b0b2a5a6c3a04855eb56f97ebd5600
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-09 18:28:36 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,22 +14,6 @@ import torch.nn.init as init
 
 
 
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
-
-
 _, term_width = os.popen('stty size', 'r').read().split()
 term_width = int(term_width)
 
