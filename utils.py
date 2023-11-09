61e19d9a090585f90dcb49289455f14af729138a
Hossein Rezaei <93883307+hossein-rezaei624@users.noreply.github.com>
2023-11-09 18:27:34 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,19 +12,7 @@ import torch.nn as nn
 import torch.nn.init as init
 
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
+
 
 def init_params(net):
     '''Init layer parameters.'''
