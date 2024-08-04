846a19133aef18f011e4cdc46710fdecd8c3639a
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-04 18:54:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -121,10 +121,10 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        ##progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-        ##             % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
       
-    print("epoch", epoch,":", 100.*correct/total)
+    print(100.*correct/total)
 
 def test(epoch):
     global best_acc
