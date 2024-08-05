98da6547800d81be3acb826f261ad1261e0d7071
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-06 00:37:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -82,11 +82,11 @@ def test(epoch):
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+            representations, logits = net(inputs)
+            loss = criterion(logits, targets)
 
             test_loss += loss.item()
-            _, predicted = outputs.max(1)
+            _, predicted = logits.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
           
@@ -101,11 +101,11 @@ def test_train(epoch):
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(trainloader_test):
             inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+            representations, logits = net(inputs)
+            loss = criterion(logits, targets)
 
             test_loss += loss.item()
-            _, predicted = outputs.max(1)
+            _, predicted = logits.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
