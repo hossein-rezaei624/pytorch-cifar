01e22909afff045bde0be17abae4215774f4b461
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-06 22:10:02 +0330

    without data augmentation in 1 (ResNet34)

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    ##transforms.RandomCrop(32, padding=4),
+    ##transforms.RandomHorizontalFlip(),
     ##transforms.RandomRotation(degrees=90),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
@@ -79,7 +79,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 
 # Model
 print('==> Building model..')
-net = ResNet18()
+net = ResNet34()
 # net = VGG('VGG19')
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -158,7 +158,7 @@ def test(epoch):
         'acc': acc,
         'epoch': epoch,
     }
-    torch.save(state, f'/home/rezaei/pytorch-cifar/checkpoint/4/ckpt{epoch}.pth')    
+    torch.save(state, f'/home/rezaei/pytorch-cifar/checkpoint/1/ckpt{epoch}.pth')    
 
 
 def test_train(epoch):
