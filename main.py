d69e764a5b0bf843999c667b41e1f4177592dff5
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-08 19:25:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 
 # Model
 print('==> Building model..')
-net = ResNet34()
+net = ResNet18()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -71,7 +71,7 @@ if device == 'cuda':
 
 criterion = nn.CrossEntropyLoss()
 
-checkpoint = torch.load('/home/rezaei/pytorch-cifar/checkpoint/resnet34_4/ckpt199.pth')
+checkpoint = torch.load('/home/rezaei/pytorch-cifar/checkpoint/resnet18_1/ckpt199.pth')
 net.load_state_dict(checkpoint['net'])
 
 # Accessing the last fully connected layer correctly
@@ -140,12 +140,15 @@ def test(epoch):
 
                 null_space_repr_target = proj_info['null_target'] @ output_vector
                 null_space_repr_non_target = proj_info['null_nontarget'] @ output_vector
+
+                target_weight = W[target_class].unsqueeze(0)  # Shape (1, 512)
+                non_target_weights = torch.cat([W[:target_class], W[target_class+1:]], dim=0)  # Shape (9, 512)
                 
     
-                col_space_repr_target_norm = torch.norm(col_space_repr_target, dim=0)/(torch.norm(proj_info['column_target'], p='fro'))
-                col_space_repr_non_target_norm = torch.norm(col_space_repr_non_target, dim=0)/(torch.norm(proj_info['column_nontarget'], p='fro'))
-                null_space_repr_target_norm = torch.norm(null_space_repr_target, dim=0)/(torch.norm(proj_info['null_target'], p='fro'))
-                null_space_repr_non_target_norm = torch.norm(null_space_repr_non_target, dim=0)/(torch.norm(proj_info['null_nontarget'], p='fro'))
+                col_space_repr_target_norm = torch.norm(col_space_repr_target, dim=0)/(torch.norm(target_weight, p='fro'))
+                col_space_repr_non_target_norm = torch.norm(col_space_repr_non_target, dim=0)/(torch.norm(non_target_weights, p='fro'))
+                null_space_repr_target_norm = torch.norm(null_space_repr_target, dim=0)/(torch.norm(target_weight, p='fro'))
+                null_space_repr_non_target_norm = torch.norm(null_space_repr_non_target, dim=0)/(torch.norm(non_target_weights, p='fro'))
 
                 col_target_list.append(col_space_repr_target_norm.item())
                 col_non_target_list.append(col_space_repr_non_target_norm.item())
@@ -203,11 +206,13 @@ def test_train(epoch):
                 null_space_repr_target = proj_info['null_target'] @ output_vector
                 null_space_repr_non_target = proj_info['null_nontarget'] @ output_vector
                 
-    
-                col_space_repr_target_norm = torch.norm(col_space_repr_target, dim=0)/(torch.norm(proj_info['column_target'], p='fro'))
-                col_space_repr_non_target_norm = torch.norm(col_space_repr_non_target, dim=0)/(torch.norm(proj_info['column_nontarget'], p='fro'))
-                null_space_repr_target_norm = torch.norm(null_space_repr_target, dim=0)/(torch.norm(proj_info['null_target'], p='fro'))
-                null_space_repr_non_target_norm = torch.norm(null_space_repr_non_target, dim=0)/(torch.norm(proj_info['null_nontarget'], p='fro'))
+                target_weight = W[target_class].unsqueeze(0)  # Shape (1, 512)
+                non_target_weights = torch.cat([W[:target_class], W[target_class+1:]], dim=0)  # Shape (9, 512)
+                
+                col_space_repr_target_norm = torch.norm(col_space_repr_target, dim=0)/(torch.norm(target_weight, p='fro'))
+                col_space_repr_non_target_norm = torch.norm(col_space_repr_non_target, dim=0)/(torch.norm(non_target_weights, p='fro'))
+                null_space_repr_target_norm = torch.norm(null_space_repr_target, dim=0)/(torch.norm(target_weight, p='fro'))
+                null_space_repr_non_target_norm = torch.norm(null_space_repr_non_target, dim=0)/(torch.norm(non_target_weights, p='fro'))
 
                 col_target_list.append(col_space_repr_target_norm.item())
                 col_non_target_list.append(col_space_repr_non_target_norm.item())
