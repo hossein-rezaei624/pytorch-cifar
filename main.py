b251179b203e618c8966eb48bdec306726a25177
Hossein Rezaei <hr505@cam.ac.uk>
2024-08-11 18:03:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,10 @@ def test(epoch):
                 target_cos_sim = cos_sim[i, target_class]
                 target_proj = projection_norms[i, target_class]
 
-                non_target_cos_sim = torch.cat((cos_sim[i, :target_class], cos_sim[i, target_class+1:]))                
+                non_target_cos_sim = torch.cat((cos_sim[i, :target_class], cos_sim[i, target_class+1:]))   
+                print("non_target_cos_sim", non_target_cos_sim)
+                aa = torch.abs(non_target_cos_sim)
+                print("torch.abs(non_target_cos_sim)", aa)
                 mean_cos_sim_nontarget = non_target_cos_sim.mean()
 
                 non_target_proj = torch.cat((projection_norms[i, :target_class], projection_norms[i, target_class+1:]))                
