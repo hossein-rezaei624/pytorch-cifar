52ac589a23be23dc69a21d564669c39fe3d2d1f4
Hossein Rezaei <hr505@cam.ac.uk>
2025-07-25 00:57:34 +0330

    Update and rename split_miniimagenet to split_miniimagenet.py

--- split_miniimagenet.py
+++ split_miniimagenet.py
@@ -1,8 +1,4 @@
-# Copyright 2022-present, Lorenzo Bonicelli, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
-# All rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
+from argparse import Namespace
 import numpy as np
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
@@ -17,12 +13,12 @@ from datasets.utils.continual_dataset import get_previous_train_loader
 from datasets.transforms.denormalization import DeNormalize
 
 
-class TinyImagenet(Dataset):
+class MiniImagenet(Dataset):
     """
-    Defines Tiny Imagenet as for the others pytorch datasets.
+    Defines Mini Imagenet as for the others pytorch datasets.
     """
-    def __init__(self, root: str, train: bool=True, transform: transforms=None,
-                target_transform: transforms=None, download: bool=False) -> None:
+    def __init__(self, root: str, train: bool = True, transform: transforms = None,
+                 target_transform: transforms = None, download: bool = False) -> None:
         self.not_aug_transform = transforms.Compose([transforms.Resize(32), transforms.ToTensor()])
         self.root = root
         self.train = train
@@ -35,24 +31,16 @@ class TinyImagenet(Dataset):
                 print('Download not needed, files already on disk.')
             else:
                 from onedrivedownloader import download
-
+                ln = "https://unimore365-my.sharepoint.com/:u:/g/personal/263133_unimore_it/EYLmey_IMdVPtGCrCBx_CCMBToexGLjdFVy5mz5mo3Wpcg?download=1"
                 print('Downloading dataset')
-                ln = "https://unimore365-my.sharepoint.com/:u:/g/personal/263133_unimore_it/EVKugslStrtNpyLGbgrhjaABqRHcE3PB_r2OEaV7Jy94oQ?e=9K29aD"
-                download(ln, filename=os.path.join(root, 'tiny-imagenet-processed.zip'), unzip=True, unzip_path=root, clean=True)
+                download(ln, filename=os.path.join(root, 'miniImagenet.zip'), unzip=True, unzip_path=root, clean=True)
 
-        self.data = []
-        for num in range(20):
-            self.data.append(np.load(os.path.join(
-                root, 'processed/x_%s_%02d.npy' %
-                      ('train' if self.train else 'val', num+1))))
-        self.data = np.concatenate(np.array(self.data))
-
-        self.targets = []
-        for num in range(20):
-            self.targets.append(np.load(os.path.join(
-                root, 'processed/y_%s_%02d.npy' %
-                      ('train' if self.train else 'val', num+1))))
-        self.targets = np.concatenate(np.array(self.targets))
+        self.data = np.load(os.path.join(
+            root, '%s_x.npy' %
+                  ('train' if self.train else 'test')))
+        self.targets = np.load(os.path.join(
+            root, '%s_y.npy' %
+                  ('train' if self.train else 'test')))
 
     def __len__(self):
         return len(self.data)
@@ -62,7 +50,7 @@ class TinyImagenet(Dataset):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(np.uint8(255 * img))
+        img = Image.fromarray(img)
         original_img = img.copy()
 
         if self.transform is not None:
@@ -77,13 +65,13 @@ class TinyImagenet(Dataset):
         return img, target
 
 
-class MyTinyImagenet(TinyImagenet):
+class MyMiniImagenet(MiniImagenet):
     """
-    Defines Tiny Imagenet as for the others pytorch datasets.
+    Defines Mini Imagenet as for the others pytorch datasets.
     """
-    def __init__(self, root: str, train: bool=True, transform: transforms=None,
-                target_transform: transforms=None, download: bool=False) -> None:
-        super(MyTinyImagenet, self).__init__(
+    def __init__(self, root: str, train: bool = True, transform: transforms = None,
+                 target_transform: transforms = None, download: bool = False) -> None:
+        super(MyMiniImagenet, self).__init__(
             root, train, transform, target_transform, download)
 
     def __getitem__(self, index):
@@ -91,7 +79,7 @@ class MyTinyImagenet(TinyImagenet):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(np.uint8(255 * img))
+        img = Image.fromarray(img)
         original_img = img.copy()
 
         not_aug_img = self.not_aug_transform(original_img)
@@ -103,30 +91,32 @@ class MyTinyImagenet(TinyImagenet):
             target = self.target_transform(target)
 
         if hasattr(self, 'logits'):
-          return img, target, not_aug_img, self.logits[index]
+            return img, target, not_aug_img, self.logits[index]
 
         return img, target, not_aug_img, index
 
 
-class SequentialTinyImagenet(ContinualDataset):
+class SequentialMiniImagenet(ContinualDataset):
 
-    NAME = 'seq-tinyimg'
+    NAME = 'seq-miniimg'
     SETTING = 'class-il'
     N_CLASSES_PER_TASK = 20
-    N_TASKS = 10
+    N_TASKS = 5
+    MEAN = (0.47313006, 0.44905752, 0.40378186)
+    STD = (0.27292014, 0.26559181, 0.27953038)
     TRANSFORM = transforms.Compose(
-            [transforms.Resize(32),
-             transforms.RandomCrop(32, padding=4),
-             transforms.RandomHorizontalFlip(),
-             transforms.ToTensor(),
-             transforms.Normalize((0.4802, 0.4480, 0.3975),
-                                  (0.2770, 0.2691, 0.2821))])
+        [transforms.Resize(32),
+         # remove data augmentation to reproduce results
+         transforms.RandomCrop(32, padding=4),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor(),
+         transforms.Normalize(MEAN,
+                              STD)])
 
     def get_examples_number(self):
-        train_dataset = MyTinyImagenet(base_path() + 'TINYIMG', train=True,
+        train_dataset = MyMiniImagenet(base_path() + 'MINIIMG', train=True,
                                   download=True)
         return len(train_dataset.data)
-
     
     def get_data_loaders(self):
         transform = self.TRANSFORM
@@ -134,54 +124,53 @@ class SequentialTinyImagenet(ContinualDataset):
         test_transform = transforms.Compose(
             [transforms.Resize(32), transforms.ToTensor(), self.get_normalization_transform()])
 
-        train_dataset = MyTinyImagenet(base_path() + 'TINYIMG',
-                                 train=True, download=True, transform=transform)
+        train_dataset = MyMiniImagenet(base_path() + 'MINIIMG',
+                                       train=True, download=True, transform=transform)
         train_dataset.not_aug_transform = test_transform  # store normalized images in the buffer
         if self.args.validation:
             train_dataset, test_dataset = get_train_val(train_dataset,
-                                                    test_transform, self.NAME)
+                                                        test_transform, self.NAME)
         else:
-            test_dataset = TinyImagenet(base_path() + 'TINYIMG',
-                        train=False, download=True, transform=test_transform)
-
+            test_dataset = MiniImagenet(base_path() + 'MINIIMG',
+                                        train=False, download=True, transform=test_transform)
         train, test = store_masked_loaders(train_dataset, test_dataset, self)
         return train, test
+
 
     def not_aug_dataloader(self, batch_size):
         transform = transforms.Compose([transforms.ToTensor(), self.get_normalization_transform()])
 
-        train_dataset = MyTinyImagenet(base_path() + 'TINYIMG', train=True,
+        train_dataset = MyMiniImagenet(base_path() + 'MINIIMG', train=True,
                                   download=True, transform=transform)
         train_loader = get_previous_train_loader(train_dataset, batch_size, self)
 
         return train_loader
+
     
     @staticmethod
     def get_backbone(args):
-        return resnet18(SequentialTinyImagenet.N_CLASSES_PER_TASK
-                        * SequentialTinyImagenet.N_TASKS, args)
+        return resnet18(SequentialMiniImagenet.N_CLASSES_PER_TASK
+                        * SequentialMiniImagenet.N_TASKS, args)
 
     @staticmethod
     def get_loss():
         return F.cross_entropy
-
 
     def get_transform(self):
         transform = transforms.Compose(  # weaken random crop to reproduce results
             [transforms.RandomCrop(32, padding=1), transforms.RandomHorizontalFlip()])
         return transform
 
-    
     @staticmethod
     def get_normalization_transform():
-        transform = transforms.Normalize((0.4802, 0.4480, 0.3975),
-                                         (0.2770, 0.2691, 0.2821))
+        transform = transforms.Normalize(SequentialMiniImagenet.MEAN,
+                                         SequentialMiniImagenet.STD)
         return transform
 
     @staticmethod
     def get_denormalization_transform():
-        transform = DeNormalize((0.4802, 0.4480, 0.3975),
-                                         (0.2770, 0.2691, 0.2821))
+        transform = DeNormalize(SequentialMiniImagenet.MEAN,
+                                SequentialMiniImagenet.STD)
         return transform
 
     @staticmethod
@@ -198,4 +187,4 @@ class SequentialTinyImagenet(ContinualDataset):
 
     @staticmethod
     def get_minibatch_size():
-        return SequentialTinyImagenet.get_batch_size()
+        return SequentialMiniImagenet.get_batch_size()
