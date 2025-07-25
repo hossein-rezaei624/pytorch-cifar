53205168eab57c5f3524178f4a8c5901f1b628df
Hossein Rezaei <hr505@cam.ac.uk>
2025-07-26 02:17:06 +0330

    Update split_miniimagenet.py

--- split_miniimagenet.py
+++ split_miniimagenet.py
@@ -1,190 +1,64 @@
-from argparse import Namespace
-import numpy as np
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset
-from backbone.ResNet18 import resnet18
-import torch.nn.functional as F
-from utils.conf import base_path_dataset as base_path
-from PIL import Image
-import os
-from datasets.utils.validation import get_train_val
-from datasets.utils.continual_dataset import ContinualDataset, store_masked_loaders
-from datasets.utils.continual_dataset import get_previous_train_loader
-from datasets.transforms.denormalization import DeNormalize
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+
+local_path='./results/split_cifar100/test1'  # set your output path
+dataset='splitcifar100'
+setting='greedy'
+data_path=''
+buffer_size=200
+alpha=4.0
+beta=0.0
+lr=2e-2
+epochs=100
+batch_size=32
+mem_batch_size=32
+use_cuda=1
+opt_type='sgd'
+seed=0
+slt_wo_aug=0
+holdout_set='sub'
+replay_mode='sub'
+use_bn=1
+limit_per_task=5000
+runner_type='coreset'
+update_mode='coreset'
+extra_data=''
+ref_train_epoch=10
+selection_steps=40
+cur_train_steps=7
+buffer_type='coreset'
+aug_type='greedy'
+ref_train_lr=3e-3
+cur_train_lr=5e-3
+ref_sample_per_task=0
 
 
-class MiniImagenet(Dataset):
-    """
-    Defines Mini Imagenet as for the others pytorch datasets.
-    """
-    def __init__(self, root: str, train: bool = True, transform: transforms = None,
-                 target_transform: transforms = None, download: bool = False) -> None:
-        self.not_aug_transform = transforms.Compose([transforms.Resize(32), transforms.ToTensor()])
-        self.root = root
-        self.train = train
-        self.transform = transform
-        self.target_transform = target_transform
-        self.download = download
-
-        if download:
-            if os.path.isdir(root) and len(os.listdir(root)) > 0:
-                print('Download not needed, files already on disk.')
-            else:
-                from onedrivedownloader import download
-                ln = "https://unimore365-my.sharepoint.com/:u:/g/personal/263133_unimore_it/EYLmey_IMdVPtGCrCBx_CCMBToexGLjdFVy5mz5mo3Wpcg?download=1"
-                print('Downloading dataset')
-                download(ln, filename=os.path.join(root, 'miniImagenet.zip'), unzip=True, unzip_path=root, clean=True)
-
-        self.data = np.load(os.path.join(
-            root, '%s_x.npy' %
-                  ('train' if self.train else 'test')))
-        self.targets = np.load(os.path.join(
-            root, '%s_y.npy' %
-                  ('train' if self.train else 'test')))
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
-        original_img = img.copy()
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        if hasattr(self, 'logits'):
-            return img, target, original_img, self.logits[index]
-
-        return img, target
-
-
-class MyMiniImagenet(MiniImagenet):
-    """
-    Defines Mini Imagenet as for the others pytorch datasets.
-    """
-    def __init__(self, root: str, train: bool = True, transform: transforms = None,
-                 target_transform: transforms = None, download: bool = False) -> None:
-        super(MyMiniImagenet, self).__init__(
-            root, train, transform, target_transform, download)
-
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
-        original_img = img.copy()
-
-        not_aug_img = self.not_aug_transform(original_img)
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        if hasattr(self, 'logits'):
-            return img, target, not_aug_img, self.logits[index]
-
-        return img, target, not_aug_img, index
-
-
-class SequentialMiniImagenet(ContinualDataset):
-
-    NAME = 'seq-miniimg'
-    SETTING = 'class-il'
-    N_CLASSES_PER_TASK = 20
-    N_TASKS = 5
-    MEAN = (0.47313006, 0.44905752, 0.40378186)
-    STD = (0.27292014, 0.26559181, 0.27953038)
-    TRANSFORM = transforms.Compose(
-        [transforms.Resize(32),
-         # remove data augmentation to reproduce results
-         transforms.RandomCrop(32, padding=4),
-         transforms.RandomHorizontalFlip(),
-         transforms.ToTensor(),
-         transforms.Normalize(MEAN,
-                              STD)])
-
-    def get_examples_number(self):
-        train_dataset = MyMiniImagenet(base_path() + 'MINIIMG', train=True,
-                                  download=True)
-        return len(train_dataset.data)
-    
-    def get_data_loaders(self):
-        transform = self.TRANSFORM
-
-        test_transform = transforms.Compose(
-            [transforms.Resize(32), transforms.ToTensor(), self.get_normalization_transform()])
-
-        train_dataset = MyMiniImagenet(base_path() + 'MINIIMG',
-                                       train=True, download=True, transform=transform)
-        train_dataset.not_aug_transform = test_transform  # store normalized images in the buffer
-        if self.args.validation:
-            train_dataset, test_dataset = get_train_val(train_dataset,
-                                                        test_transform, self.NAME)
-        else:
-            test_dataset = MiniImagenet(base_path() + 'MINIIMG',
-                                        train=False, download=True, transform=test_transform)
-        train, test = store_masked_loaders(train_dataset, test_dataset, self)
-        return train, test
-
-
-    def not_aug_dataloader(self, batch_size):
-        transform = transforms.Compose([transforms.ToTensor(), self.get_normalization_transform()])
-
-        train_dataset = MyMiniImagenet(base_path() + 'MINIIMG', train=True,
-                                  download=True, transform=transform)
-        train_loader = get_previous_train_loader(train_dataset, batch_size, self)
-
-        return train_loader
-
-    
-    @staticmethod
-    def get_backbone(args):
-        return resnet18(SequentialMiniImagenet.N_CLASSES_PER_TASK
-                        * SequentialMiniImagenet.N_TASKS, args)
-
-    @staticmethod
-    def get_loss():
-        return F.cross_entropy
-
-    def get_transform(self):
-        transform = transforms.Compose(  # weaken random crop to reproduce results
-            [transforms.RandomCrop(32, padding=1), transforms.RandomHorizontalFlip()])
-        return transform
-
-    @staticmethod
-    def get_normalization_transform():
-        transform = transforms.Normalize(SequentialMiniImagenet.MEAN,
-                                         SequentialMiniImagenet.STD)
-        return transform
-
-    @staticmethod
-    def get_denormalization_transform():
-        transform = DeNormalize(SequentialMiniImagenet.MEAN,
-                                SequentialMiniImagenet.STD)
-        return transform
-
-    @staticmethod
-    def get_scheduler(model, args):
-        return None
-
-    @staticmethod
-    def get_epochs():
-        return 50
-
-    @staticmethod
-    def get_batch_size():
-        return 32
-
-    @staticmethod
-    def get_minibatch_size():
-        return SequentialMiniImagenet.get_batch_size()
+python3 -u offline_continual_learning.py --local_path=$local_path \
+	--dataset=$dataset \
+	--setting=$setting \
+	--data_path=$data_path \
+	--buffer_size=$buffer_size \
+	--alpha=$alpha \
+	--beta=$beta \
+	--lr=$lr \
+	--epochs=$epochs \
+	--batch_size=$batch_size \
+	--mem_batch_size=$mem_batch_size \
+	--use_cuda=$use_cuda \
+	--opt_type=$opt_type \
+	--seed=$seed \
+	--slt_wo_aug=$slt_wo_aug \
+	--holdout_set=$holdout_set \
+	--replay_mode=$replay_mode \
+	--use_bn=$use_bn \
+	--limit_per_task=$limit_per_task \
+	--runner_type=$runner_type \
+	--update_mode=$update_mode \
+	--extra_data=$extra_data \
+	--ref_train_epoch=$ref_train_epoch \
+	--selection_steps=$selection_steps \
+	--cur_train_steps=$cur_train_steps \
+	--ref_train_lr=$ref_train_lr \
+	--cur_train_lr=$cur_train_lr \
+	--buffer_type=$buffer_type \
+	--ref_sample_per_task=$ref_sample_per_task \
+	--aug_type=$aug_type
