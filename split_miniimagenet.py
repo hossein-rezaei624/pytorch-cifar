c3190a8d176d88052cd62c7fa15497d92b421c3d
Hossein Rezaei <hr505@cam.ac.uk>
2025-10-12 12:19:15 +0330

    Update split_miniimagenet.py

--- split_miniimagenet.py
+++ split_miniimagenet.py
@@ -1,104 +1,71 @@
-"""
-Author: Yonglong Tian (yonglong@mit.edu)
-Date: May 07, 2020
-"""
-from __future__ import print_function
-
 import torch
-import torch.nn as nn
+from utils.buffer import Buffer
+from utils.args import *
+from models.utils.continual_model import ContinualModel
 
 
-class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
-    def __init__(self, temperature=0.07, contrast_mode='all'):
-        super(SupConLoss, self).__init__()
-        self.temperature = temperature
-        self.contrast_mode = contrast_mode
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser(description='CGR: Confidence-Guided Reply for Buffer-Based Continual Learning')
+    add_management_args(parser)
+    add_experiment_args(parser)
+    add_rehearsal_args(parser)
+    
+    return parser
 
-    def forward(self, features, labels=None, mask=None):
-        """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
+    
+class Cgr(ContinualModel):
+    NAME = 'cgr'
+    COMPATIBILITY = ['class-il', 'task-il']
 
-        Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
-            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
-                has the same class as sample i. Can be asymmetric.
-        Returns:
-            A loss scalar.
-        """
-        device = (torch.device('cuda')
-                  if features.is_cuda
-                  else torch.device('cpu'))
+    def __init__(self, backbone, loss, args, transform):
+        super(Cgr, self).__init__(backbone, loss, args, transform)
+        self.buffer = Buffer(self.args.buffer_size, self.device)
+        self.task = 0
+        self.epoch = 0
+    
+    def begin_task(self, dataset, train_loader):
+        self.epoch = 0
+        self.task += 1
+    
+    def end_epoch(self, dataset, train_loader):
+        self.epoch += 1            
 
-        if len(features.shape) < 3:
-            raise ValueError('`features` needs to be [bsz, n_views, ...],'
-                             'at least 3 dimensions are required')
-        if len(features.shape) > 3:
-            features = features.view(features.shape[0], features.shape[1], -1)
-
-        batch_size = features.shape[0]
-        if labels is not None and mask is not None:
-            raise ValueError('Cannot define both `labels` and `mask`')
-        elif labels is None and mask is None:
-            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:
-            labels = labels.contiguous().view(-1, 1)
-            if labels.shape[0] != batch_size:
-                raise ValueError('Num of labels does not match num of features')
-            mask = torch.eq(labels, labels.T).float().to(device)
+    def observe(self, inputs, labels, not_aug_inputs, index_):
+        
+        real_batch_size = inputs.shape[0]
+        
+        # batch update
+        batch_x, batch_y = inputs, labels
+        batch_x = batch_x.to(self.device)
+        batch_y = batch_y.to(self.device)
+        batch_x_combine = batch_x
+        batch_y_combine = batch_y
+            
+        self.opt.zero_grad()    
+        
+        if self.buffer.is_empty():
+            logits = self.net(batch_x_combine)
+            novel_loss = self.loss(logits, batch_y_combine)
+            
         else:
-            mask = mask.float().to(device)
+            mem_x, mem_y = self.buffer.get_data(
+                self.args.minibatch_size, transform=self.transform)
+        
+            mem_x = mem_x.to(self.device)
+            mem_y = mem_y.to(self.device)
+            mem_x_combine = mem_x
+            mem_y_combine = mem_y
 
-        contrast_count = features.shape[1]
-        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
-        if self.contrast_mode == 'one':
-            anchor_feature = features[:, 0]
-            anchor_count = 1
-        elif self.contrast_mode == 'all':
-            anchor_feature = contrast_feature
-            anchor_count = contrast_count
-        elif self.contrast_mode == 'proxy':
-            anchor_feature = features[:, 0]
-            contrast_feature = features[:, 1]
-            anchor_count = 1
-            contrast_count = 1
-        else:
-            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
+            combined_inputs = torch.cat([mem_x_combine, batch_x_combine])
+            combined_labels = torch.cat((mem_y_combine, batch_y_combine))
 
-        # compute logits
-        anchor_dot_contrast = torch.div(
-            torch.matmul(anchor_feature, contrast_feature.T),
-            self.temperature)
-        # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-        logits = anchor_dot_contrast - logits_max.detach()
+            combined_logits = self.net(combined_inputs)
+            novel_loss = self.loss(combined_logits, combined_labels)
+        
+        novel_loss.backward()
+        self.opt.step()
 
-        # tile mask
-        mask = mask.repeat(anchor_count, contrast_count)
-        # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
-            1,
-            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
-            0
-        )
-
-        # compute log_prob
-        if self.contrast_mode == 'proxy':
-            exp_logits = torch.exp(logits)
-        else:
-            mask = mask * logits_mask
-            exp_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
-
-        # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-
-        # loss
-        loss = -1 * mean_log_prob_pos
-        loss = loss.view(anchor_count, batch_size).mean()
-
-        return loss
+        self.buffer.add_data(examples=not_aug_inputs[:real_batch_size],
+                             labels=labels[:real_batch_size])
+        
+        return novel_loss.item()
