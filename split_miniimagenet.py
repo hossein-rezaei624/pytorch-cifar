f80ded1deea13d7ce2dcfd4b3dbcaa29abca8094
Hossein Rezaei <hr505@cam.ac.uk>
2026-05-19 22:55:00 +0330

    Update split_miniimagenet.py

--- split_miniimagenet.py
+++ split_miniimagenet.py
@@ -1,195 +1,64 @@
-# -*-coding:utf8-*-
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-import argparse
-import os
-import numpy as np
-
-import utils
-from continual_learning import continual_runner
-from dataset import idataset
-
-from datetime import datetime
-
-
-def make_selection_params(opts):
-    selection_params = {
-        'init_size': 0,
-        'class_balance': False,
-        'only_new_data': True,
-        'cur_train_steps': opts.cur_train_steps,
-        'cur_train_lr': opts.cur_train_lr,
-        'selection_steps': opts.selection_steps,
-        'ideal_logit': True,
-        'logit_compute_mode': 'end_task',
-        'loss_params': {
-            'ce_factor': 1.0,
-            'mse_factor': opts.slt_mse_factor
-        },
-        'ref_train_params': {
-            'lr': opts.ref_train_lr,
-            'epochs': opts.ref_train_epoch,
-            'batch_size': 32,
-            'eval_batch_size': 20,
-            'use_cuda': bool(opts.use_cuda),
-            'early_stop': -1,
-            'log_steps': 100,
-            'opt_type': 'sgd',
-            'loss_params': {
-                'ce_factor': 1.0,
-                'mse_factor': 0.0
-            },
-            'ref_sample_per_task': opts.ref_sample_per_task  # default 0
-        }
-    }
-    return selection_params
+local_path='./results/split_cifar100/test0'  # set your output path
+dataset='splitcifar100'
+setting='der'
+data_path=''
+buffer_size=1000
+alpha=4.0
+beta=0.0
+lr=2e-2
+epochs=50
+batch_size=32
+mem_batch_size=32
+use_cuda=1
+opt_type='sgd'
+seed=0
+slt_wo_aug=0
+holdout_set='sub'
+replay_mode='sub'
+use_bn=1
+limit_per_task=5000
+runner_type='coreset'
+update_mode='coreset'
+extra_data=''
+ref_train_epoch=10
+selection_steps=40
+cur_train_steps=7
+buffer_type='coreset'
+aug_type='der'
+ref_train_lr=3e-3
+cur_train_lr=5e-3
+ref_sample_per_task=200
 
 
-def main(opts):
-
-    start_hossein = datetime.now()
-    
-    if not os.path.exists(opts.local_path):
-        os.makedirs(opts.local_path)
-    # make data loaders
-    model_params, transforms, eval_transforms, task_dic, train_loaders, train_sub_loaders_wo_aug, test_loaders =\
-        idataset.get_dataset(
-            opts=opts
-        )
-    # modify selection params
-    selection_params = make_selection_params(opts=opts)
-    for k in selection_params.keys():
-        print(k, '\t\t', selection_params[k])
-    # build continual runner
-    train_params = {
-        'lr': opts.lr,
-        'alpha': opts.alpha,
-        'epochs': opts.epochs,
-        'batch_size': opts.batch_size,
-        'mem_batch_size': opts.mem_batch_size,
-        'eval_batch_size': 20,
-        'use_cuda': bool(opts.use_cuda),
-        'early_stop': -1,
-        'log_steps': 100,
-        'opt_type': opts.opt_type
-    }
-    if opts.beta > 0:
-        train_params['beta'] = opts.beta
-    if opts.runner_type == 'coreset':
-        runner = continual_runner.ContinualRunner(
-            local_path=opts.local_path,
-            model_params=model_params,
-            transforms=transforms,
-            train_params=train_params,
-            selection_params=selection_params,
-            use_cuda=bool(opts.use_cuda),
-            task_dic=task_dic,
-            buffer_size=opts.buffer_size,
-            seed=opts.seed,
-            replay_mode=opts.replay_mode,
-            selection_transforms=eval_transforms if bool(opts.slt_wo_aug) else None,
-            extra_data_mode=opts.extra_data.split(','),
-            buffer_type=opts.buffer_type
-        )
-    else:
-        raise ValueError('Invalid runner type')
-    # continual training and update memory
-    for i in range(len(task_dic)):
-        if opts.runner_type == 'coreset':
-            accs = runner.train_single_task(opts.dataset,
-                train_loader=train_loaders[i],
-                eval_loaders=test_loaders,
-                verbose=True,
-                do_evaluation=True
-            )
-        else:
-            raise ValueError('Invalid runner type')
-        ##print('accuracies on testset after task', i, 'is:', accs, np.mean(accs))
-        if opts.runner_type == 'coreset':
-            runner.update_buffer(
-                full_train_loader=train_sub_loaders_wo_aug[i],
-                sub_loader=train_sub_loaders_wo_aug[i],
-                next_loader=None
-            )
-        else:
-            raise ValueError('Invalid runner type')
-        runner.next_task(dump_buffer=True)
-
-
-    end_hossein = datetime.now()
-    print(f"Elapsed time: {end_hossein - start_hossein}")
-
-
-if __name__ == '__main__':
-    """
-    selection parameters are added in slt_config.py
-    """
-    parser = argparse.ArgumentParser('offline continual learning')
-    parser.add_argument('--local_path', type=str)
-    parser.add_argument('--data_path', type=str, default='')
-    parser.add_argument('--dataset', type=str)
-    parser.add_argument('--setting', type=str, default='der')
-    parser.add_argument('--buffer_size', type=int)
-    parser.add_argument('--alpha', type=float)
-    parser.add_argument('--beta', type=float, default=0)
-    parser.add_argument('--lr', type=float)
-    parser.add_argument('--epochs', type=int)
-    parser.add_argument('--batch_size', type=int)
-    parser.add_argument('--mem_batch_size', type=int)
-    parser.add_argument('--use_cuda', type=int)
-    parser.add_argument('--opt_type', type=str)
-    parser.add_argument('--slt_wo_aug', type=int, default=0)
-    parser.add_argument('--holdout_set', type=str, default='full')
-    parser.add_argument('--replay_mode', type=str, default='full')
-    parser.add_argument('--use_bn', type=int, default=0)
-    parser.add_argument('--limit_per_task', type=int, default=1000)
-    parser.add_argument('--runner_type', type=str, default='coreset')
-    parser.add_argument('--update_mode', type=str, default='coreset')
-    parser.add_argument('--extra_data', type=str, default='')
-    parser.add_argument('--slt_mse_factor', type=float, default=-1)
-    parser.add_argument('--cur_train_steps', type=int, default=-1)
-    parser.add_argument('--ref_train_epoch', type=int, default=-1)
-    parser.add_argument('--selection_steps', type=int, default=-1)
-    parser.add_argument('--ref_train_lr', type=float, default=-1)
-    parser.add_argument('--cur_train_lr', type=float, default=-1)
-    parser.add_argument('--aug_type', type=str, default='der')
-    parser.add_argument('--buffer_type', type=str, default='coreset')
-    parser.add_argument('--ref_sample_per_task', type=int, default=-1)
-    parser.add_argument('--seed', type=int)
-    parser.add_argument('--label_noise', type=float, default=0.0,
-                        help='Symmetric label-noise rate in [0,1] (rebuttal experiment). 0 = no noise.')
-    args = parser.parse_args()
-
-    utils.set_random_seed(seed=args.seed)
-    print('script\t\t', 'offline_continual_learning.py')
-    print('local path\t\t', args.local_path)
-    print('data path\t\t', args.data_path)
-    print('dataset\t\t', args.dataset)
-    print('setting\t\t', args.setting)
-    print('buffer size\t\t', args.buffer_size)
-    print('alpha\t\t', args.alpha)
-    print('beta\t\t', args.beta)
-    print('lr\t\t', args.lr)
-    print('epochs\t\t', args.epochs)
-    print('batch size\t\t', args.batch_size)
-    print('mem batch size\t\t', args.mem_batch_size)
-    print('opt type\t\t', args.opt_type)
-    print('select without augmentation\t\t', args.slt_wo_aug)
-    print('holdout set\t\t', args.holdout_set)
-    print('replay mode\t\t', args.replay_mode)
-    print('use bn\t\t', args.use_bn)
-    print('limit per task\t\t', args.limit_per_task)
-    print('runner type\t\t', args.runner_type)
-    print('update mode\t\t', args.update_mode)
-    print('extra data\t\t', args.extra_data)
-    print('slt_mse_factor\t\t', args.slt_mse_factor)
-    print('cur_train_steps\t\t', args.cur_train_steps)
-    print('ref_train_epoch\t\t', args.ref_train_epoch)
-    print('selection steps\t\t', args.selection_steps)
-    print('ref train lr\t\t', args.ref_train_lr)
-    print('cur train lr\t\t', args.cur_train_lr)
-    print('aug type\t\t', args.aug_type)
-    print('buffer type\t\t', args.buffer_type)
-    print('ref sample per task\t\t', args.ref_sample_per_task)
-    print('seed\t\t', args.seed)
-    print('label noise\t\t', args.label_noise)
-    main(opts=args)
+python3 -u offline_continual_learning.py --local_path=$local_path \
+	--dataset=$dataset \
+	--setting=$setting \
+	--data_path=$data_path \
+	--buffer_size=$buffer_size \
+	--alpha=$alpha \
+	--beta=$beta \
+	--lr=$lr \
+	--epochs=$epochs \
+	--batch_size=$batch_size \
+	--mem_batch_size=$mem_batch_size \
+	--use_cuda=$use_cuda \
+	--opt_type=$opt_type \
+	--seed=$seed \
+	--slt_wo_aug=$slt_wo_aug \
+	--holdout_set=$holdout_set \
+	--replay_mode=$replay_mode \
+	--use_bn=$use_bn \
+	--limit_per_task=$limit_per_task \
+	--runner_type=$runner_type \
+	--update_mode=$update_mode \
+	--extra_data=$extra_data \
+	--ref_train_epoch=$ref_train_epoch \
+	--selection_steps=$selection_steps \
+	--cur_train_steps=$cur_train_steps \
+	--ref_train_lr=$ref_train_lr \
+	--cur_train_lr=$cur_train_lr \
+	--buffer_type=$buffer_type \
+	--ref_sample_per_task=$ref_sample_per_task \
+	--aug_type=$aug_type
