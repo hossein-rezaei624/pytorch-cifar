76e77436bb337becfd9d661ba71d9d9c1c954d72
Hossein Rezaei <hr505@cam.ac.uk>
2026-08-10 10:13:19 +0330

    Update split_miniimagenet.sh

--- split_miniimagenet.sh
+++ split_miniimagenet.sh
@@ -1,103 +1,54 @@
-import torch
-from utils.buffer import Buffer
-from utils.args import *
-from models.utils.continual_model import ContinualModel
-from utils.pcr_loss import SupConLoss
-from utils.pcr_transforms_aug import transforms_aug
-
-import torch.nn as nn
-import numpy as np
-##import matplotlib.pyplot as plt
-import torchvision
-
-
-def get_parser() -> ArgumentParser:
-    parser = ArgumentParser(description='PCR.')
-    add_management_args(parser)
-    add_experiment_args(parser)
-    add_rehearsal_args(parser)
-    
-    return parser
-
-
-class Pcr(ContinualModel):
-    NAME = 'pcr'
-    COMPATIBILITY = ['class-il']
-
-    def __init__(self, backbone, loss, args, transform):
-        super(Pcr, self).__init__(backbone, loss, args, transform)
-        self.buffer = Buffer(self.args.buffer_size, self.device)
-        self.task = 0
-        self.epoch = 0
-
-    def begin_train(self, dataset):
-        self.n_sample_per_task = dataset.get_examples_number()//dataset.N_TASKS
-    
-    def begin_task(self, dataset, train_loader):
-        self.epoch = 0
-        self.task += 1
-    
-    def end_epoch(self, dataset, train_loader):
-        self.epoch += 1
-        
-
-    def observe(self, inputs, labels, not_aug_inputs, index_):
-        
-        real_batch_size = inputs.shape[0]
-        
-
-        # batch update
-        batch_x, batch_y = inputs, labels
-        batch_x_aug = torch.stack([transforms_aug[self.args.dataset](batch_x[idx].cpu())
-                                   for idx in range(batch_x.size(0))])
-        batch_x = batch_x.to(self.device)
-        batch_x_aug = batch_x_aug.to(self.device)
-        batch_y = batch_y.to(self.device)
-        batch_x_combine = torch.cat((batch_x, batch_x_aug))
-        batch_y_combine = torch.cat((batch_y, batch_y))
-            
-        logits, feas= self.net.pcrForward(batch_x_combine)
-        novel_loss = 0*self.loss(logits, batch_y_combine)
-        self.opt.zero_grad()
-    
-        
-        if not self.buffer.is_empty():
-            mem_x, mem_y = self.buffer.get_data(
-                self.args.minibatch_size, transform=self.transform)
-        
-            mem_x_aug = torch.stack([transforms_aug[self.args.dataset](mem_x[idx].cpu())
-                                     for idx in range(mem_x.size(0))])
-            mem_x = mem_x.to(self.device)
-            mem_x_aug = mem_x_aug.to(self.device)
-            mem_y = mem_y.to(self.device)
-            mem_x_combine = torch.cat([mem_x, mem_x_aug])
-            mem_y_combine = torch.cat([mem_y, mem_y])
-
-
-            mem_logits, mem_fea= self.net.pcrForward(mem_x_combine)
-
-            combined_feas = torch.cat([mem_fea, feas])
-            combined_labels = torch.cat((mem_y_combine, batch_y_combine))
-            combined_feas_aug = self.net.pcrLinear.L.weight[combined_labels]
-
-            combined_feas_norm = torch.norm(combined_feas, p=2, dim=1).unsqueeze(1).expand_as(combined_feas)
-            combined_feas_normalized = combined_feas.div(combined_feas_norm + 0.000001)
-
-            combined_feas_aug_norm = torch.norm(combined_feas_aug, p=2, dim=1).unsqueeze(1).expand_as(
-                combined_feas_aug)
-            combined_feas_aug_normalized = combined_feas_aug.div(combined_feas_aug_norm + 0.000001)
-            cos_features = torch.cat([combined_feas_normalized.unsqueeze(1),
-                                      combined_feas_aug_normalized.unsqueeze(1)],
-                                     dim=1)
-            PSC = SupConLoss(temperature=0.09, contrast_mode='proxy')
-            novel_loss += PSC(features=cos_features, labels=combined_labels)
-
-        
-        novel_loss.backward()
-        self.opt.step()
-
-
-        self.buffer.add_data(examples=not_aug_inputs[:real_batch_size],
-                             labels=labels[:real_batch_size])
-        
-        return novel_loss.item()
+absl-py==2.3.1
+annotated-types==0.7.0
+appdirs==1.4.4
+beautifulsoup4==4.15.0
+certifi==2026.4.22
+charset-normalizer==3.4.7
+click==8.1.8
+docker-pycreds==0.4.0
+eval_type_backport==0.3.1
+filelock==3.16.1
+flatbuffers==2.0.7
+frozendict==2.4.7
+gdown==5.2.2
+gitdb==4.0.12
+GitPython==3.1.50
+higher==0.2.1
+idna==3.15
+imageio==2.35.1
+jax==0.2.19
+jaxlib==0.1.70+cuda102
+lazy_loader==0.4
+networkx==3.1
+neural-tangents==0.3.8
+numpy==1.24.3
+onedrivedownloader==1.1.3
+opencv-python==4.13.0.92
+opt_einsum==3.4.0
+packaging==26.2
+Pillow==9.3.0
+pkg_resources==0.0.0
+platformdirs==4.3.6
+protobuf==4.25.9
+psutil==7.2.2
+pydantic==2.10.6
+pydantic_core==2.27.2
+PySocks==1.7.1
+PyWavelets==1.4.1
+PyYAML==6.0.3
+requests==2.32.4
+scikit-image==0.21.0
+scipy==1.10.1
+sentry-sdk==2.60.0
+setproctitle==1.3.3
+six==1.16.0
+smmap==5.0.3
+soupsieve==2.7
+tifffile==2023.7.10
+torch==1.8.1+cu102
+torchvision==0.9.1+cu102
+tqdm==4.68.4
+typing_extensions==4.13.2
+urllib3==2.2.3
+Wand==0.6.13
+wandb==0.23.0
